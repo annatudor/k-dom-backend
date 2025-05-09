3b36d8f1092c1822fd6a5b9099bac60d3d7e1c2f
Ana Tudor <tudorana.at21@gmail.com>
2025-05-09 22:59:49 +0300

    project intialization with .gitignore configurated

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace KDomBackend.Data
+{
+    public class DatabaseContext
+    {
+        private readonly string _connectionString;
+        public DatabaseContext(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using KDomBackend.Data;
+using KDomBackend.Helpers;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+builder.Services.AddSingleton<DatabaseContext>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
